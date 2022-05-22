9a6374a3e8725d1a64869aa790fb27f4e094f744
Sakshi Shah <sakshi.shah299@gmail.com>
2022-05-22 16:57:27 +0530

    no changes

---
+++ 01-starting-project/src/components/Users/AddUsers.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AddUsers = props =>{
+return(
+    <form>
+<label >Username</label>
+<input id="username" type="text"/>
+</form>
+);
+};
+
+export default AddUsers;
